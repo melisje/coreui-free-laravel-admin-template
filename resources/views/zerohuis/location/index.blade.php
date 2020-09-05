f44cdc22665f4063749cc76e49d7ce43fb9a8913
Jef Melis <jef.melis@melit.be>
2020-09-05 14:16:50 +0200

    added colom to the frontpage preview page of location update column headers

--- resources/views/zerohuis/location/index.blade.php
+++ resources/views/zerohuis/location/index.blade.php
@@ -15,24 +15,31 @@
                 <thead>
                     <tr>
                         <th>#</th>
-                        <th>id</th>
-                        <th>name</th>
-                        <th>street/th>
-                        <th>zipcode</th>
-                        <th>location</th>
+                        <th><i class="cil-qr-code"></i> Id</th>
+                        <th><i class="cil-user"></i> Name</th>
+                        <th><i class="cil-location-pin"></i> Street</th>
+                        <th><i class="cil-location-pin"></i> Zipcode</th>
+                        <th><i class="cil-location-pin"></i> Location</th>
+                        <th><i class="cil-find-in-page"></i></th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach( $locations as $location)
                         <tr>
                             <td>
-                                <a href="{{ route('location.show',$location) }}" class="text-dark"><i class="cil-qr-code"></i></a>
+                                <a href="{{ route('admin.location.show',$location) }}" class="text-dark"><i
+                                        class="cil-print"></i></a>
                             </td>
                             <td>{{ $location->id }}</td>
                             <td>{{ $location->name }}</td>
                             <td>{{ $location->street }}</td>
                             <td>{{ $location->zipcode }}</td>
                             <td>{{ $location->location }}</td>
+                            <td>
+                                <a href="{{ route('location.show',$location) }}" class="text-body" target="_blank">
+                                    <i class="cil-find-in-page"></i>
+                                </a>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
