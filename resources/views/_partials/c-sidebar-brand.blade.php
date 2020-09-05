244d123b4fdb8fdc63c2b9d72cecaf08aaa4779a
Jef Melis <jef.melis@melit.be>
2020-09-05 13:52:28 +0200

    updated size of minimized brand icon

--- resources/views/_partials/c-sidebar-brand.blade.php
+++ resources/views/_partials/c-sidebar-brand.blade.php
@@ -2,7 +2,7 @@
     <img alt="Zerohuis Logo" width="118" height="46"
          class="c-sidebar-brand-full"
          src="@yield('c-sidebar-brand-full',asset('assets/brand/coreui-base-white.svg')) ">
-    <img alt="Zerohuis Logo" width="118" height="46"
+    <img alt="Zerohuis Logo" width="30" height="46"
          class="c-sidebar-brand-minimized"
          src="@yield('c-sidebar-brand-minimized',asset('assets/brand/coreui-signet-white.svg')) ">
 </div>
