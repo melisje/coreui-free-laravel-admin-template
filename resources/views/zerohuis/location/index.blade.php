815a919642e438308249a690427682ed3c28013d
Jef Melis <jef.melis@melit.be>
2020-09-05 14:18:19 +0200

    embeded table in responsive div

--- resources/views/zerohuis/location/index.blade.php
+++ resources/views/zerohuis/location/index.blade.php
@@ -11,40 +11,41 @@
     <div class="container-fluid">
         <div class="fade-in">
 
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th><i class="cil-qr-code"></i> Id</th>
-                        <th><i class="cil-user"></i> Name</th>
-                        <th><i class="cil-location-pin"></i> Street</th>
-                        <th><i class="cil-location-pin"></i> Zipcode</th>
-                        <th><i class="cil-location-pin"></i> Location</th>
-                        <th><i class="cil-find-in-page"></i></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach( $locations as $location)
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
                         <tr>
-                            <td>
-                                <a href="{{ route('admin.location.show',$location) }}" class="text-dark"><i
-                                        class="cil-print"></i></a>
-                            </td>
-                            <td>{{ $location->id }}</td>
-                            <td>{{ $location->name }}</td>
-                            <td>{{ $location->street }}</td>
-                            <td>{{ $location->zipcode }}</td>
-                            <td>{{ $location->location }}</td>
-                            <td>
-                                <a href="{{ route('location.show',$location) }}" class="text-body" target="_blank">
-                                    <i class="cil-find-in-page"></i>
-                                </a>
-                            </td>
+                            <th>#</th>
+                            <th><i class="cil-qr-code"></i> Id</th>
+                            <th><i class="cil-user"></i> Name</th>
+                            <th><i class="cil-location-pin"></i> Street</th>
+                            <th><i class="cil-location-pin"></i> Zipcode</th>
+                            <th><i class="cil-location-pin"></i> Location</th>
+                            <th><i class="cil-find-in-page"></i></th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
+                    </thead>
+                    <tbody>
+                        @foreach( $locations as $location)
+                            <tr>
+                                <td>
+                                    <a href="{{ route('admin.location.show',$location) }}" class="text-dark"><i
+                                            class="cil-print"></i></a>
+                                </td>
+                                <td>{{ $location->id }}</td>
+                                <td>{{ $location->name }}</td>
+                                <td>{{ $location->street }}</td>
+                                <td>{{ $location->zipcode }}</td>
+                                <td>{{ $location->location }}</td>
+                                <td>
+                                    <a href="{{ route('location.show',$location) }}" class="text-body" target="_blank">
+                                        <i class="cil-find-in-page"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
